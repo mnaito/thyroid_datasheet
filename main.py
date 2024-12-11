1c937b52f5de9dbe88118a348b263f0edc9257fb
mnaito <86825760+mnaito@users.noreply.github.com>
2024-12-11 14:22:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import streamlit as st
 import data_input
 import analysis
 
+st.set_page_config(
+    page_title='Datasheet for temporal thyroid monitoring',
+)
+
 tab1,tab2=st.tabs(['入力','まとめ'])
 
 with tab1:
